2dbda0016ce706d4081d059815aa2ff3b2e38111
Adrian Diez Canseco <adrian10cr@proton.me>
2025-05-16 18:42:01 -0400

    include image

--- app/src/main/java/com/example/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/example/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -5,12 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.horoscapp.databinding.FragmentHoroscopeBinding
+import com.example.horoscapp.domain.model.HoroscopeInfo.*
+import com.example.horoscapp.domain.model.HoroscopeModel
+import com.example.horoscapp.ui.horoscope.adapter.HoroscopeAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
+
+    private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
+    private lateinit var horoscopeAdapter: HoroscopeAdapter
 
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +38,46 @@ class HoroscopeFragment : Fragment() {
     ): View {
         _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    private fun initUI() {
+        initList()
+        initUIState()
+    }
+
+    private fun initList() {
+        horoscopeAdapter = HoroscopeAdapter(onItemSelected = {
+            val type = when (it) {
+                Aquarius -> HoroscopeModel.Aquarius
+                Aries -> HoroscopeModel.Aries
+                Cancer -> HoroscopeModel.Cancer
+                Capricorn -> HoroscopeModel.Capricorn
+                Gemini -> HoroscopeModel.Gemini
+                Leo -> HoroscopeModel.Leo
+                Libra -> HoroscopeModel.Libra
+                Pisces -> HoroscopeModel.Pisces
+                Sagittarius -> HoroscopeModel.Sagittarius
+                Scorpio -> HoroscopeModel.Scorpio
+                Taurus -> HoroscopeModel.Taurus
+                Virgo -> HoroscopeModel.Virgo
+            }
+            findNavController().navigate(HoroscopeFragmentDirections.actionHoroscopeFragmentToHoroscopeDetailActivity(type))
+        })
+        binding.rvHoroscope.apply {
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = horoscopeAdapter
+        }
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeViewModel.horoscope.collect {
+                    // Log.i("Demo", it.toString())
+                    horoscopeAdapter.updateList(it)
+                }
+            }
+        }
     }
 
 }
