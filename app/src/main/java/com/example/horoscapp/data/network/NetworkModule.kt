81712ff2bf4066b13cd6ad44f4bac150f525ecfd
Adrian Diez Canseco <adrian10cr@proton.me>
2025-05-17 11:43:30 -0400

    build variants

---
+++ app/src/main/java/com/example/horoscapp/data/network/NetworkModule.kt
@@ -0,0 +1,49 @@
+package com.example.horoscapp.data.network
+
+import com.example.horoscapp.BuildConfig.BASE_URL
+import com.example.horoscapp.data.RepositoryImpl
+import com.example.horoscapp.domain.Repository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+    }
+
+    @Provides
+    fun provideHoroscopeApiService(retrofit: Retrofit): HoroscopeApiService {
+        return retrofit.create(HoroscopeApiService::class.java)
+    }
+
+    @Provides
+    fun provideRepository(apiService: HoroscopeApiService): Repository {
+        return RepositoryImpl(apiService)
+    }
+}
